a5ec53aa6ef2f4ad4fb3ac653ed8d2cb810baa3d
Татьяна Красненкова <tatanakrasnenkova@MacBook-Air-Tatana.local>
2022-12-03 21:14:44 +0300

    вообще ничего не понятно с этой задачей

--- main3.py
+++ main3.py
@@ -3,18 +3,3 @@
 print("Введите номер плоскости для уточнения возможных координат")
 ploskost = int (input ())
 
-import math
-math.inf
-
-if ploskost == 1:
-    print('x = [0, +', math.inf, ')')
-    print('y = [0, +', math.inf, ')')
-if ploskost == 2:
-    print('x = [0, -', math.inf, ')')
-    print('y = [0, +', math.inf, ')')
-if ploskost == 3:
-    print('x = [0, -', math.inf, ')')
-    print('y = [0, -', math.inf, ')')
-if ploskost == 4:
-    print('x = [0, +', +math.inf, ')')
-    print('y = [0, -', +math.inf, ')')
