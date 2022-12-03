da858251da0f2f334c4308db37bdbde739a37a1d
Татьяна Красненкова <tatanakrasnenkova@MacBook-Air-Tatana.local>
2022-12-03 20:42:23 +0300

    file 5 save

---
+++ main5.py
@@ -0,0 +1,9 @@
+#Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+print('x, y, z')
+
+
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print(x,y,z, not(x or y or z) == (not x and not y and not z) )
