d8581bc18a39acfcf65695f41b39f37dbd7b61c9
Татьяна Красненкова <tatanakrasnenkova@MacBook-Air-Tatana.local>
2022-12-07 21:01:01 +0300

    task1

--- python_sem2/main1.py
+++ python_sem2/main1.py
@@ -4,5 +4,10 @@
 
 #- пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-number = int(input())
+n = int(input())
+sum = 1
 
+for i in range(1, n+1):
+    sum = sum * i
+    print(sum)
+    
