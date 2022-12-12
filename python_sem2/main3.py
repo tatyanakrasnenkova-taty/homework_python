c1acbb61ae4b4627c900efe556e4a04f293c17b0
Татьяна Красненкова <tatanakrasnenkova@MacBook-Air-Tatana.local>
2022-12-12 21:02:48 +0300

    проба решения

---
+++ python_sem2/main3.py
@@ -0,0 +1,34 @@
+#Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+
+#for i in result:
+    #if(i != b):
+        #ist.append(i)
+    #else:
+        #i += i 
+
+num = list(input())
+res = 0
+#ist = []
+# while (int(x) == x):
+    #if(True):
+        #x = x * 10
+    #else:
+        #break
+
+
+
+
+for i in range(len(num)):
+    res+=int(num[i].replace('.', '0'))
+print(res)
+
+
+
+
+
+#for i in x:
+    #ist.append(i)
+
+#print(type(ist))
+#print(sum(ist))
+
