f419093183696c56b1ef2776b25346db47e0e7b4
tatyanakrasnenkova-taty <104003796+tatyanakrasnenkova-taty@users.noreply.github.com>
2022-12-14 19:22:08 +0300

    Update main.py

--- python_sem2/main.py
+++ python_sem2/main.py
@@ -1,7 +1,7 @@
 '''
 5 Реализуйте алгоритм перемешивания списка.
 Из библиотеки random использовать можно только randint
-'''
+
 
 from random import sample
  
@@ -11,3 +11,23 @@ if __name__ == '__main__':
  
     l = sample(nums, len(nums))
     print(l)
+'''   
+   
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+
+import random
+def mix_list(list_original):
+    list = list_original[:]
+    list_length = len(list)
+    for i in range(list_length):
+        index_aleatory = random.randint(0, list_length - 1)
+        temp = list[i]
+        list[i] = list[index_aleatory]
+        list[index_aleatory] = temp
+    return list
+
+mixed_list = mix_list(list)
+print(list)
+print(mixed_list)
