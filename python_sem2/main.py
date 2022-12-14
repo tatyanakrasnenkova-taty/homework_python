8d5c0a63001a451d5a6c5ca504733b7f9a9fe921
Татьяна Красненкова <tatanakrasnenkova@MacBook-Air-Tatana.local>
2022-12-14 19:09:31 +0300

    maybe/so so

---
+++ python_sem2/main.py
@@ -0,0 +1,13 @@
+'''
+5 Реализуйте алгоритм перемешивания списка.
+Из библиотеки random использовать можно только randint
+'''
+
+from random import sample
+ 
+if __name__ == '__main__':
+ 
+    nums = range(5)
+ 
+    l = sample(nums, len(nums))
+    print(l)
